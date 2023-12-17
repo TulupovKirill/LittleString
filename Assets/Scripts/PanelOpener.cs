bd811b6a08a487b218ecfef7c41289b0dce49453
ZXClolopo <lolopo3457@gmail.com>
2023-12-17 20:52:38 +0600

    Complite Scene Guitar

--- Assets/Scripts/PanelOpener.cs
+++ Assets/Scripts/PanelOpener.cs
@@ -11,13 +11,13 @@ public class PanelOpener : MonoBehaviour
 
     public void Start() {
         panels = new List<GameObject>() {
-        GameObject.Find("Panel"),
-        GameObject.Find("Panel1"),
-        GameObject.Find("Panel2"),
-        GameObject.Find("Panel3"),
-        GameObject.Find("Panel4"),
-        GameObject.Find("Panel5"),
-        GameObject.Find("Panel6"),
+        GameObject.Find("/Canvas/RawImage/Panel"),
+        GameObject.Find("/Canvas/RawImage/Panel1"),
+        GameObject.Find("/Canvas/RawImage/Panel2"),
+        GameObject.Find("/Canvas/RawImage/Panel3"),
+        GameObject.Find("/Canvas/RawImage/Panel4"),
+        GameObject.Find("/Canvas/RawImage/Panel5"),
+        GameObject.Find("/Canvas/RawImage/Panel6"),
         };
         panels.Remove(panel);
     }
